2a25458fce0468fc0c54f9b792f1a2c3f70df89e
Denis <den24.03@mail.ru>
2024-03-09 13:46:13 +0500

    add HomeWork5

---
+++ HomeWork/HomeWork_5/app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<ProductDetails :product="product"/>
+</template>
+
+<script>
+import ProductDetails from './components/ProductDetails.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    ProductDetails
+},
+data() {
+  return {
+    product: 
+    {
+      name: 'Product1',
+      price: 99.99,
+      available: true
+    }
+  }
+},
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ HomeWork/HomeWork_5/app/src/components/ProductDetails.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>{{ product.name }}</h2>
+    <p>{{ formattedPrice }}</p>
+    <p>{{ product.available ? 'Available' : 'Out of stock' }}</p>
+  </div>
+</template>
+
+<script>
+ export default {
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    formattedPrice() {
+      const price = Number(this.product.price).toFixed(2);
+      return `$${price}`;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
